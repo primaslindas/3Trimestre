f7184fb07293cd93c667057f6b111dcb94627d1d
primaslindas <130568572+primaslindas@users.noreply.github.com>
2023-10-18 09:40:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function tocaSomPom(){
-document.querySelector('.tecla_pom').play(); 
-}
+   document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_pom').onclick = tocaSomPom;}
+
+function tocaSomClap(){
+   document.querySelector('#som_tecla_clap').play();
+document.querySelector('tecla_clap').onclick = tocaSomclap;}
+
+function tocaSomTim(){
+    document.querySelector ('#som_tecla_tim').play();
+document.querySelector('.tecla_tim').onclick = tocaSomTim;}
+
