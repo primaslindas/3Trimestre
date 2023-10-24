2a0b227c697024086c4fac4b3f6bd7bdceae0310
primaslindas <130568572+primaslindas@users.noreply.github.com>
2023-10-24 08:50:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-function tocaSomPom(){
-   document.querySelector('#som_tecla_pom').play();
-document.querySelector('.tecla_pom').onclick = tocaSomPom;}
+function tocaSom(idElementoAudio){
+   document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas=document.querySelectorAll('.tecla');
 
-function tocaSomClap(){
-   document.querySelector('#som_tecla_clap').play();
-document.querySelector('tecla_clap').onclick = tocaSomclap;}
+//Estrutura de repetição - Enquanto 
+for( let contador = 0; contador < listaDeTeclas.leight; contador++){
+   const tecla = listaDeTeclas[contador];
+   const instrumento = tecla.classList [1];
+   const idAudio = `#som_ ${instrumento}`;
+   console.log (idAudio);
 
-function tocaSomTim(){
-    document.querySelector ('#som_tecla_tim').play();
-document.querySelector('.tecla_tim').onclick = tocaSomTim;}
+   tecla.onclick = function(){
+      tocaSom(idAudio);
+   };
+   tecla.onkeydown = function(){
+      tecla.classList.add('ativa');
+   }
+   tecla.onkeyup = function(){
+      tecla.classList.remove('ativa');
+   }
+   
+ console.log (contador);
+}
+
 
