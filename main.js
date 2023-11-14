ba2c734bacb6a9cf070301b0dc92da24149450cf
primaslindas <130568572+primaslindas@users.noreply.github.com>
2023-11-14 08:48:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 function tocaSom(idElementoAudio){
-   document.querySelector(idElementoAudio).play();
+   const elemento = document.querySelector(idElementoAudio);
+   if(elemento === null){
+      alert('Elemento nao encontrado');
+   }
+   if (elemento && elemento.localName === 'audio'){
+   elemento.play();
+   }
 }
-const listaDeTeclas=document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//Estrutura de repetição - Enquanto 
+//Estrutura de repeticao - Enquanto 
 for( let contador = 0; contador < listaDeTeclas.leight; contador++){
    const tecla = listaDeTeclas[contador];
    const instrumento = tecla.classList [1];
    const idAudio = `#som_ ${instrumento}`;
+
    console.log (idAudio);
 
    tecla.onclick = function(){
       tocaSom(idAudio);
    };
-   tecla.onkeydown = function(){
-      tecla.classList.add('ativa');
-   }
+   tecla.onkeydown = function(evento){
+      console.log(evento.code);
+      if(evento.code ==='Enter'|| evento.code ==='Space'){
+      tecla.classList.add('ativa')
+      }
+   };
    tecla.onkeyup = function(){
       tecla.classList.remove('ativa');
-   }
-   
+   };
+   console.log(instrumento);
  console.log (contador);
 }
 
